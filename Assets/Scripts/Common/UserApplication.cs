c8c81064dfffe8f3e06873f07810c2d079923316
sugarsan <143979030+sugarsan@users.noreply.github.com>
2023-09-13 14:18:08 +0900

    project3 9/13

--- Assets/Scripts/Common/UserApplication.cs
+++ Assets/Scripts/Common/UserApplication.cs
@@ -48,6 +48,56 @@ public class UserApplication : MonoBehaviour
             return S._fixCharaManager;
         }
     }
+    
+    public UserDataManager _userDataManager;
+    static public UserDataManager userDataManager
+    {
+        get
+        {
+            if (S._userDataManager == null)
+            {
+                S._userDataManager = GameObject.Find("UserDataManager").GetComponent<UserDataManager>();
+            }
+            return S._userDataManager;
+        }
+    }
 
+    public CharaGridRenderer _charaGridRenderer;
+    static public CharaGridRenderer charaGridRenderer
+    {
+        get
+        {
+            if (S._charaGridRenderer == null)
+            {
+                S._charaGridRenderer = GameObject.Find("UIParts_CharaGridRenderer").GetComponent<CharaGridRenderer>();
+            }
+            return S._charaGridRenderer;
+        }
+    }
 
+    public PHPConnectManager _phpConnectManager;
+    static public PHPConnectManager phpConnectManager
+    {
+        get
+        {
+            if (S._phpConnectManager == null)
+            {
+                S._phpConnectManager = GameObject.Find("PHPConnectManager").GetComponent<PHPConnectManager>();
+            }
+            return S._phpConnectManager;
+        }
+    }
+
+    public P3_UserDataManager _p3_userDataManager;
+    static public P3_UserDataManager p3_userDataManager
+    {
+        get
+        {
+            if (S._p3_userDataManager == null)
+            {
+                S._p3_userDataManager = GameObject.Find("UserDataManager").GetComponent<P3_UserDataManager>();
+            }
+            return S._p3_userDataManager;
+        }
+    }
 }
